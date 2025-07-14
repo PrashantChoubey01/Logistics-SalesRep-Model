ebf0ff336f3ee095a58047328650477fdb6a75da
Prashant Choubey <prashant.choubey@dpworld.com>
2025-07-15 00:41:36 +0400

    saving,, not working properly
    rebasing the data

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,5 +1,6 @@
 """Agents package initialization"""
 
 from .base_agent import BaseAgent
+from .email_processing_pipeline import EmailProcessingPipeline
 
-__all__ = ["BaseAgent"]
+__all__ = ["BaseAgent", "EmailProcessingPipeline"]
